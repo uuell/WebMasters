5b4263d3c9e7d4ccaeab148ac93e161fa0e9f5bd
king <112244652+uuell@users.noreply.github.com>
2023-09-10 12:53:11 +0800

    added a menu button

--- src/components/NavbarItems.jsx
+++ src/components/NavbarItems.jsx
@@ -1,10 +1,23 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRightLong } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 import "../styling/navbar.css";
 
-console.log(screen.width);
-
 function NavbarItems() {
+
+  const [class_menu, set_class_menu] = useState("");
+  
+  function setClass(event) {
+    let open_menu = event.currentTarget.classList.contains('open-menu')
+    let close_menu = event.currentTarget.classList.contains('close-menu')
+
+    if (!open_menu) {
+      set_class_menu("open-menu")
+    } else if(!close_menu && open_menu) {
+      set_class_menu("close-menu")
+    }
+  }
+
   return (
     <div id="navbar-items-container">
       <ul>
@@ -22,6 +35,10 @@ function NavbarItems() {
         Start a Project
         <FontAwesomeIcon className="arrow-right" icon={faArrowRightLong} />
       </button>
+      <div id="menu" className={class_menu} onClick={setClass}>
+        <div className="menu-btn bar-1"></div>
+        <div className="menu-btn bar-2"></div>
+      </div>
     </div>
   );
 }
