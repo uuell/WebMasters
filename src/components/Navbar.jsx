735b9a5567879c744feb5c9278518b4f5deb4130
king <112244652+uuell@users.noreply.github.com>
2023-09-21 16:56:29 +0800

    navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,45 @@
 import NavbarItems from "./NavbarItems";
-import icon from "../assets/webmastersicon.png";
+import { useState } from "react";
+import {useRef, useEffect} from 'react';
 
 function Navbar() {
+
+  const ref = useRef(null);
+
+  const [class_menu, set_class_menu] = useState("");
+  const [navClass, setNavClass] = useState("");
+
+  function handleClick(event) {
+    let open_menu = event.currentTarget.classList.contains('open-menu');
+    let close_menu = event.currentTarget.classList.contains('close-menu');
+    
+    if (!open_menu) {
+      set_class_menu("open-menu");
+    } else if(!close_menu && open_menu) {
+      set_class_menu("close-menu");
+    }
+    const navbar = ref.current;
+    if(navbar.className === "" || navbar.className == "animate-up") {
+      setNavClass("animate-down")
+    } else if(navbar.className == "animate-down") {
+      setNavClass("animate-up")
+    }
+  }
+
   return (
-    <nav id="navbar">
-      <div id="title-container">
-        <a href="#showcase">
-          <h1 id="title">
-            Web<em>Masters</em>
-          </h1>
-        </a>
-      </div>
-      <NavbarItems />
+    <nav ref={ref} id="navbar" className={navClass}>
+        <div id="title-container">
+          <a href="#showcase">
+            <h1 id="title">
+              Web<em>Masters</em>
+            </h1>
+          </a>
+        </div>
+        <NavbarItems />
+        <div id="menu" className={class_menu} onClick={handleClick}>
+          <div className="menu-btn bar-1"></div>
+          <div className="menu-btn bar-2"></div>
+        </div>
     </nav>
   );
 }
--- src/components/NavbarItems.jsx
+++ src/components/NavbarItems.jsx
@@ -1,26 +1,13 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRightLong } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
 import "../styling/navbar.css";
 
-function NavbarItems() {
+function NavbarItems( ) {
 
-  const [class_menu, set_class_menu] = useState("");
-  
-  function setClass(event) {
-    let open_menu = event.currentTarget.classList.contains('open-menu')
-    let close_menu = event.currentTarget.classList.contains('close-menu')
-
-    if (!open_menu) {
-      set_class_menu("open-menu")
-    } else if(!close_menu && open_menu) {
-      set_class_menu("close-menu")
-    }
-  }
 
   return (
     <div id="navbar-items-container">
-      <ul >
+      <ul>
         <a href="#work">
           <li className="navbar-items">Work</li>
         </a>
@@ -35,10 +22,6 @@ function NavbarItems() {
         Start a Project
         <FontAwesomeIcon className="arrow-right" icon={faArrowRightLong} />
       </button>
-      <div id="menu" className={class_menu} onClick={setClass}>
-        <div className="menu-btn bar-1"></div>
-        <div className="menu-btn bar-2"></div>
-      </div>
     </div>
   );
 }
